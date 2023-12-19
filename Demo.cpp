87d6907f5ab1212368d177775ce39e314ba8c3b4
Dhananjay Yelwande <yelwandedhananjay@gmail.com>
2023-12-19 08:49:17 +0530

    new file:   Demo 	new file:   Demo.bash 	new file:   Demo.c 	new file:   Demo.class 	new file:   Demo.cpp 	new file:   Demo.cs 	new file:   Demo.exe 	new file:   Demo.java 	new file:   Demo.js 	new file:   Demo.py

---
+++ Demo.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+
+class HelloWorld {
+public:
+    HelloWorld() {
+        cout << "Hello, World!" << endl;
+    }
+};
+
+int main() {
+    HelloWorld hello;
+    return 0;
+}
+
---
+++ Demo.cs
@@ -0,0 +1,6 @@
+using System;
+public class Demo{
+    public static void Main(){
+        Console.WriteLine("Hello World!");
+    }
+}
